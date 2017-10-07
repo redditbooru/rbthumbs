58939a2d8d4d3df1e7c06fd362c39b05630a0cf0
Matt Hackmann <matt@dxprog.com>
2017-10-07 00:16:30 -0700

    added request-promise-native and refactored for that

--- src/image-fetcher.js
+++ src/image-fetcher.js
@@ -1,28 +1,8 @@
 const bluebird = require('bluebird');
 const hostImageResolver = require('host-image-resolver');
 const fs = require('fs');
-const request = require('request');
+const request = require('request-promise-native');
 const nodeUrl = require('url');
-
-/**
- * Async wrapper for request
- *
- * @param {*} options
- * @return {Promise} A promise that resolves to a buffer of the request body
- */
-function requestAsync(options) {
-  return new Promise((resolve, reject) => {
-    request(options, (err, res, body) => {
-      if (err) {
-        reject(err);
-      } else if (res.statusCode !== 200 || !body) {
-        reject(new Error(`Server failed with HTTP code ${res.statusCode}`));
-      } else {
-        resolve(Buffer.from(body));
-      }
-    });
-  });
-}
 
 /**
  * Downloads an image, doing host resolultion and "hot-link evasion"
@@ -32,7 +12,7 @@ function requestAsync(options) {
  */
 module.exports = function fetch(url) {
   return hostImageResolver(url)
-    .then(urls => requestAsync({
+    .then(urls => request({
       url: urls[0],
       encoding: null,
       // A scummy way to get around hotlinking images/responses
