f6c27ca0385bd0c6a51a3fe6a17da9658963b7d4
Matt Hackmann <matt@dxprog.com>
2021-03-20 19:10:02 -0700

    fuck you, bluebird

--- tests/helpers.js
+++ tests/helpers.js
@@ -1,4 +1,17 @@
+import { identify } from 'imagemagick';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
 export const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export function identifyAsync(path) {
+  return new Promise((resolve, reject) => {
+    identify(path, (err, features) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(features);
+      }
+    })
+  });
+}
